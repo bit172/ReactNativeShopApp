6267a38d69c4cf7ef009af60e6ec1575c7e1213c
Jonathan Hsu <bit172@gmail.com>
2020-01-14 19:24:17 -0500

    Added shop data

---
+++ screens/shop-screen/shop.data.js
@@ -0,0 +1,67 @@
+export const SHOP_DATA = [
+  {
+    id: 1,
+    name: "Pen",
+    description:
+      "Writing instrument used to apply ink to a surface, usually paper, for writing or drawing",
+    price: 2
+  },
+  {
+    id: 2,
+    name: "Notebook",
+    description:
+      "A book used for recording notes, writing, drawing or scrapbooking",
+    price: 5
+  },
+  {
+    id: 3,
+    name: "Laptop",
+    description: "A small, portable personal computer",
+    price: 1000
+  },
+  {
+    id: 4,
+    name: "Hammer",
+    description:
+      "A tool that is swung to deliver an impact to a small area of an object",
+    price: 30
+  },
+  {
+    id: 5,
+    name: "Shovel",
+    description:
+      "A tool for digging, lifting, and moving bulk materials, such as soil, coal, gravel, snow, sand, or ore",
+    price: 45
+  },
+  {
+    id: 6,
+    name: "Scissors",
+    description: "A hand-operated shearing tool",
+    price: 10
+  },
+  {
+    id: 7,
+    name: "Watch",
+    description: "A timepiece intended to be carried or worn by a person",
+    price: 350
+  },
+  {
+    id: 8,
+    name: "Yeezy 700",
+    description: "Hype Beast things.",
+    price: 700
+  },
+  {
+    id: 9,
+    name: "Gold Pen",
+    description:
+      "Writing instrument used to apply ink to a surface, usually paper, for writing or drawing made of gold",
+    price: 200
+  },
+  {
+    id: 10,
+    name: "Meme",
+    description: "Sample Text",
+    price: 1337
+  }
+];
